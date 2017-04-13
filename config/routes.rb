ecdb2169e24424b4a5861a684403a6fba3c21e5c
Daniel Tea <daniel_tea@spe.sony.com>
2017-04-13 00:55:02 -0700

    Adds room channel and hook up coffeescript form submit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'messages#index'
+
   resources :users
   resources :messages
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  mount ActionCable.server, at: '/cable'
 end
